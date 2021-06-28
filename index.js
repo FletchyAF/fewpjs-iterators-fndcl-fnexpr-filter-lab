5314236b1f0429a82a5fa5d4c63ae0f47632cc07
FletchyAF <joshua.f1998@gmail.com>
2021-06-28 11:45:28 -0400

    commit tree c803b7cd74bd4b5fdd16daf1cb0eec9601d39255

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter( match =>
+      match.toLowerCase() === sought.toLowerCase() 
+      )
+}
+
+function fuzzyMatch(source, sought) {
+    return source.filter( match =>
+        match.toLowerCase().indexOf(sought.toLowerCase()) === 0 
+    )
+}
+
+function matchName(source, sought) {
+    return source.filter( record => record.name === sought)
+  }
